93f7db51ba1cee9570612016985bc0ece0800157
Farah Masood <farah.masood@sjsu.edu>
2023-03-26 03:13:34 -0700

    navigation, clean up code and replace icons

--- app/src/main/java/edu/sjsu/android/habiteamproject/HomePage.java
+++ app/src/main/java/edu/sjsu/android/habiteamproject/HomePage.java
@@ -1,13 +1,50 @@
 package edu.sjsu.android.habiteamproject;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment home = new HomeFragment();
+        Fragment calendar = new CalendarFragment();
+        Fragment notif = new NotificationFragment();
+        Fragment setting = new SettingFragment();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, home).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    selectedFragment = home;
+                } else if (itemId == R.id.calendar) {
+                    selectedFragment = calendar;
+                } else if (itemId == R.id.notifications) {
+                    selectedFragment = notif;
+                } else if (itemId == R.id.settings) {
+                    selectedFragment = setting;
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                }
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/sjsu/android/habiteamproject/SettingFragment.java
@@ -0,0 +1,47 @@
+package edu.sjsu.android.habiteamproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingFragment extends Fragment {
+
+    private Button signOutButton;
+    public SettingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        signOutButton = view.findViewById(R.id.signout);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+        return view;
+    }
+}
