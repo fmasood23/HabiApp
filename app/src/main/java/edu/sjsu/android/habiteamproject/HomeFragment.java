869e036d2b72362020fd53a74cd9065b0fde149e
Farah Masood <farah.masood@sjsu.edu>
2023-04-01 16:57:02 -0700

    cleanup homefragment (trackers) onCreateView

--- app/src/main/java/edu/sjsu/android/habiteamproject/HomeFragment.java
+++ app/src/main/java/edu/sjsu/android/habiteamproject/HomeFragment.java
@@ -35,61 +35,19 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-//        NavController controller = NavHostFragment.findNavController(this);
-////
-//        view.findViewById(R.id.water_button).setOnClickListener(nv ->
-//                NavHostFragment.findNavController(this).navigate(R.id.action_homeFragment_to_waterTracker));
+        //on click listeners for tracker pages
+        view.findViewById(R.id.water_button).setOnClickListener(v ->
+                getParentFragmentManager().beginTransaction().replace(R.id.fragment_container, new WaterTracker()).addToBackStack(null).commit());
 
-        view.findViewById(R.id.water_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final FragmentTransaction ft = getParentFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_container, new WaterTracker());
-                ft.addToBackStack(null);
-                ft.commit();
+        view.findViewById(R.id.to_do_button).setOnClickListener(v ->
+                getParentFragmentManager().beginTransaction().replace(R.id.fragment_container, new ToDoList()).addToBackStack(null).commit());
 
-            }
-        });
+        view.findViewById(R.id.sleep_button).setOnClickListener(v ->
+                getParentFragmentManager().beginTransaction().replace(R.id.fragment_container, new SleepTracker()).addToBackStack(null).commit());
 
-        view.findViewById(R.id.to_do_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final FragmentTransaction ft = getParentFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_container, new ToDoList());
-                ft.addToBackStack(null);
-                ft.commit();
-            }
-        });
+        view.findViewById(R.id.grocery_button).setOnClickListener(v ->
+                getParentFragmentManager().beginTransaction().replace(R.id.fragment_container, new GroceryList()).addToBackStack(null).commit());
 
-        view.findViewById(R.id.to_do_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final FragmentTransaction ft = getParentFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_container, new ToDoList());
-                ft.addToBackStack(null);
-                ft.commit();
-            }
-        });
-
-        view.findViewById(R.id.sleep_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final FragmentTransaction ft = getParentFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_container, new SleepTracker());
-                ft.addToBackStack(null);
-                ft.commit();
-            }
-        });
-
-        view.findViewById(R.id.grocery_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final FragmentTransaction ft = getParentFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_container, new GroceryList());
-                ft.addToBackStack(null);
-                ft.commit();
-            }
-        });
         return view;
     }
 
